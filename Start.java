613331bcc65da2563f9881e965e2624ceaa89b39
George101905 <georgeguerra45@gmail.com>
2023-10-25 11:30:58 -0500

    10/25/23

--- Start.java
+++ Start.java
@@ -2,11 +2,18 @@ public class Start {
     public static void main(String[] args){
         try{
             IntVector test = new IntVector(-1000);
+
             System.out.println("Size: " + test.size());
 
             for(int i = 0; i < test.size();i++){
-
+                test.add(i);
             }
+
+            System.out.println(test);
+
+            test.add(0, 50);
+
+            test.remove(0);
         } catch(IllegalArgumentException ex) {
             System.out.println("That's illegal");
         } catch(ArithmeticException ex){
